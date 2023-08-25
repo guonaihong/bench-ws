6a73e2ffafc5ddc9dea32f482b1d8ff1f18b9b0f
guonaihong <guonaihong@qq.com>
2023-08-25 13:10:24 +0800

    up

--- script/tcpkali-benchmark.sh
+++ script/tcpkali-benchmark.sh
@@ -2,9 +2,22 @@
 SEC="30s"
 SLEEP_SEC="40"
 
-echo "quickws.bufio:"
+echo "quickws.delay.12x:"
 killall quickws.linux &>/dev/null
-./quickws.linux -u --addr ":9000" &
+#./quickws.linux --use-delay-write --addr ":9000" &
+./quickws.linux -b 12 --use-delay-write -a ":9000" --delay-write-init-buffer-size 11264 &
+PID=$!
+sleep 1
+tcpkali -c 10000 --connect-rate 10000 -r 10000 -T 30s -f 1K.txt --ws 127.0.0.1:9000/
+kill $PID
+# 让cpu 温度降些，防止过热，影响后观框架的测试, 散热好的, sleep时间可以改短些
+sleep $SLEEP_SEC
+echo ""
+echo "quickws.delay.8x:"
+killall quickws.linux &>/dev/null
+
+#./quickws.linux --use-delay-write --addr ":9000" &
+./quickws.linux -b 8 --use-delay-write -a ":9000" --delay-write-init-buffer-size 11264 &
 PID=$!
 sleep 1
 tcpkali -c 10000 --connect-rate 10000 -r 10000 -T 30s -f 1K.txt --ws 127.0.0.1:9000/
@@ -13,10 +26,32 @@ kill $PID
 sleep $SLEEP_SEC
 echo ""
 
-echo "quickws.delay:"
+echo "quickws.tcp-delay:"
 killall quickws.linux &>/dev/null
 #./quickws.linux --use-delay-write --addr ":9000" &
-./quickws.linux --use-delay-write -a ":9000" --delay-write-init-buffer-size 11264 &
+./quickws.linux -o -a ":9000"  &
+PID=$!
+sleep 1
+tcpkali -c 10000 --connect-rate 10000 -r 10000 -T 30s -f 1K.txt --ws 127.0.0.1:9000/
+kill $PID
+# 让cpu 温度降些，防止过热，影响后观框架的测试, 散热好的, sleep时间可以改短些
+sleep $SLEEP_SEC
+echo ""
+
+echo "quickws.delay.tcp-delay.windows.8x:"
+killall quickws.linux &>/dev/null
+./quickws.linux -o -b 8 --use-delay-write -a ":9000" --delay-write-init-buffer-size 11264 &
+PID=$!
+sleep 1
+tcpkali -c 10000 --connect-rate 10000 -r 10000 -T 30s -f 1K.txt --ws 127.0.0.1:9000/
+kill $PID
+# 让cpu 温度降些，防止过热，影响后观框架的测试, 散热好的, sleep时间可以改短些
+sleep $SLEEP_SEC
+echo ""
+
+echo "quickws.bufio:"
+killall quickws.linux &>/dev/null
+./quickws.linux -u --addr ":9000" &
 PID=$!
 sleep 1
 tcpkali -c 10000 --connect-rate 10000 -r 10000 -T 30s -f 1K.txt --ws 127.0.0.1:9000/
