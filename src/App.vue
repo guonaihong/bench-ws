96e8e6175de3902110fba4b615178947072f0ac4
guonaihong <guonaihong@qq.com>
2024-04-19 00:02:31 +0800

    更新

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <line-chart />
+  </div>
+</template>
+
+<script>
+import LineChart from './components/LineChart.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LineChart
+  }
+}
+</script>
+
+<style>
+/* Add any global styles here */
+</style>
+
---
+++ src/components/LineChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <canvas ref="chartCanvas"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  mounted() {
+    this.renderChart();
+  },
+  methods: {
+    renderChart() {
+      const ctx = this.$refs.chartCanvas.getContext('2d');
+      this.chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  },
+  beforeUnmount() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add any component-specific styles here */
+</style>
+
